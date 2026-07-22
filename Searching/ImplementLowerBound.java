b08d736324d559323dea22743ddd014a9c89fcaa
GunjanSengar <gunjansengar444@gmail.com>
2026-07-23 00:42:29 +0530

    Implement Lower Bound

---
+++ Searching/ImplementLowerBound.java
@@ -0,0 +1,17 @@
+class Solution {
+    int lowerBound(int[] arr, int target) {
+        int ans=arr.length; int left=0; int right=arr.length-1;
+        while(left<=right){
+            int mid=(left+right)/2;
+            if(arr[mid]>=target){
+                ans=mid;
+                right=mid-1; 
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return ans;
+        
+    }
+}
