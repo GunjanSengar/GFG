2f0acd97a478180a7b4811516494cd8955563e95
GunjanSengar <gunjansengar444@gmail.com>
2026-07-23 00:19:51 +0530

    Sorted Insert Position

---
+++ Searching/SortedInsertPosition.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsertK(int arr[], int k) {
+int left=0; int right=arr.length-1;
+while(left<=right){
+    int mid=(left+right)/2;
+    if(arr[mid]<k){
+        left=mid+1;
+        
+    }
+    else{
+        right=mid-1;
+    }
+}
+return left;
+
+
+
+    }
+}
