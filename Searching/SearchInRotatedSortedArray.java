a1c559f344a4cec74a6ecf0fc1c047d17df5f938
GunjanSengar <gunjansengar444@gmail.com>
2026-07-24 00:35:06 +0530

    Search in Rotated Sorted Array

---
+++ Searching/SearchInRotatedSortedArray.java
@@ -0,0 +1,29 @@
+class Solution {
+    int search(int[] arr, int target) {
+int left=0; int right=arr.length-1;
+while(left<=right){
+    int mid=(left+right)/2;
+    if(arr[mid]==target){
+        return mid;
+    }
+    if(arr[left]<=arr[mid]){
+    if(target<arr[mid]&& target>=arr[left]){
+        right=mid-1;
+        
+    } else{
+        left=mid+1;
+        
+    }
+    
+}
+else{
+    if(target>arr[mid] && target<=arr[right]){
+        left=mid+1;
+    } else{
+        right=mid-1;
+    }
+}
+
+    } return -1;
+}
+}
